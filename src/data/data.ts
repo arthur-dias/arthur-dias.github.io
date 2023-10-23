6464bd888eb760a23950924717fabc5f52ad810b
Arthur Ribeiro Dias <dias.arthurr@gmail.com>
2023-10-23 16:02:41 -0300

    Update data.ts

--- src/data/data.ts
+++ src/data/data.ts
@@ -33,10 +33,17 @@ export const resume = [
 
 export const personalProjects = [
   {
-    project_name: 'Que presente?',
+    project_name: 'E-commerce dashboard',
     project_description:
-      'Uma aplicação que utiliza a OpenAI Api para gerar ideias de presentes para familiares e amigos baseado nos seus hobbies. Construido com Vite, React e Typescript e com testes em Cypress.',
-    project_website: 'https://que-presente.netlify.app/',
-    projetc_repo: 'https://github.com/arthur-dias/whatgift-app',
+      'SPA de um dashboard para e-commerce. Feito com Next.js, com Tailwind e Shadcn/ui. Autenticação feita com Clerk Auth. Tamném utiliza Zustand para gerenciamento de estado. Como banco de dados, utiliza MySQL com Prisma e PlanetScale. Para gerenciamento de imagens utiliza Cloudinary e para pagamentos Stripe. Dentro do dashboard há opção para criação e edição de múltiplas lojas, adicionar, editar e remover banners, categorias, tamanhos, cores e produtos, estes com opções para múltiplas imagens, para marcação como destaque e para arquivamento, e registro de pedidos, todos pesquisáveis. Ainda há opção para troca de tema e gerenciamento de conta. No dashboard é possível visualizar a quantidade de produtos vendidos, o lucro total e o número de produtos cadastrados, tal qual como um gráfico com as vendas mensais. Também disponibiliza as rotas API para acesso externo de cada um dos items. ',
+    project_website: 'https://ecomm-admin-six.vercel.app/',
+    projetc_repo: 'https://github.com/arthur-dias/ecomm-admin',
+  },
+  {
+    project_name: 'E-commerce loja',
+    project_description:
+      'Frontend de uma loja de roupas. Feito com Next.js, com Tailwind e Shadcn/ui. Também utiliza Zustand para gerenciamento de estado. Na loja é possível adicionar e demover items do carrinho e filtrar dentro de cada categoria. Também é possível proceder e finalizar o pagamento via Stripe.',
+    project_website: 'https://ecomm-store-olive.vercel.app/',
+    projetc_repo: 'https://github.com/arthur-dias/ecomm-store',
   },
 ]
